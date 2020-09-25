3b4e51ef556b3f0c8e8dcc24fd7e2c45ba0bd2fb
Anna Rau <annarau21@yahoo.com>
2020-09-25 17:27:29 -0500

    setting up food and drinks

--- shared/drinks.js
+++ shared/drinks.js
@@ -1,10 +1,158 @@
 export const DRINKS = [
-    {
+    { //Start hot Drinks
         id: 0,
         name: "Plain Coffee",
+        image: 'components\images\Drinks\coffee.jpg',
+        type: "hot",
+        price: "5 / 7",
+        sizes: "8 oz / 12 oz"
+    },
+    {
+        id: 1,
+        name: "Latte",
+        image: 'components\images\Drinks\coffee.jpg',
+        type: "hot",
+        price: "5 / 7",
+        sizes: "8 oz / 12 oz"
+    },
+    {
+        id: 2,
+        name: "Macchiato",
         image: '',
-        description: '',
-        price: 5
+        type: "hot",
+        price: "3",
+        sizes: "4 oz"
+    },
+    {
+        id: 3,
+        name: "Pour Over",
+        image: '',
+        type: "hot",
+        price: "3 / 4",
+        sizes: "8 oz / 12 oz"
+    },
+    {
+        id: 4,
+        name: "Tea Latte",
+        image: '',
+        type: "hot",
+        price: "5",
+        sizes: "12 oz"
+    },
+    {
+        id: 5,
+        name: "Hot Chocolate",
+        image: '',
+        type: "hot",
+        price: "4 / 5",
+        sizes: "8 oz / 12 oz"
+    },
+    {
+        id: 6,
+        name: "Mocha",
+        image: '',
+        type: "hot",
+        price: "5 / 6",
+        sizes: "8 oz / 12 oz"
+    },
+    {
+        id: 7,
+        name: "Americano",
+        image: '',
+        type: "hot",
+        price: "3",
+        sizes: "12 oz"
+    },
+    {
+        id: 8,
+        name: "Cappuccino",
+        image: '',
+        type: "hot",
+        price: "4",
+        sizes: "6 oz"
+    },
+    {
+        id: 9,
+        name: "Espresso",
+        image: '',
+        type: "hot",
+        price: "3",
+        sizes: "3 oz"
+    },
+    {
+        id: 10,
+        name: "Tea",
+        image: '',
+        type: "hot",
+        price: "3",
+        sizes: "12 oz"
+    },
+    {
+        id: 11,
+        name: "Chai Latte",
+        image: '',
+        type: "hot",
+        price: "4 / 5",
+        sizes: "8 oz / 12 oz"
+    },
+    { // Start Cold Drinks
+        id: 12,
+        name: "Cold Brew",
+        image: '',
+        type: "cold",
+        price: "5",
+        sizes: "16 oz"
+    },
+    { 
+        id: 13,
+        name: "Iced Latte",
+        image: '',
+        type: "cold",
+        price: "5",
+        sizes: "16 oz"
+    },{ 
+        id: 14,
+        name: "Iced Tea",
+        image: '',
+        type: "cold",
+        price: "3",
+        sizes: "16 oz"
+    },{ 
+        id: 15,
+        name: "Iced Americano",
+        image: '',
+        type: "cold",
+        price: "3",
+        sizes: "16 oz"
+    },{ 
+        id: 16,
+        name: "Iced Chai Latte",
+        image: '',
+        type: "cold",
+        price: "5",
+        sizes: "16 oz"
+    },{ 
+        id: 17,
+        name: "Iced Mocha",
+        image: '',
+        type: "cold",
+        price: "5",
+        sizes: "16 oz"
+    },{ 
+        id: 18,
+        name: "Iced Caramel Macchiato",
+        image: '',
+        type: "cold",
+        price: "5",
+        sizes: "16 oz"
+    },
+    { 
+        id: 19,
+        name: "Iced Plain Coffee",
+        image: '',
+        type: "cold",
+        price: "5 / 7",
+        sizes: "8 oz / 12 oz"
     }
 
 ]
--- shared/food.js
+++ shared/food.js
@@ -3,8 +3,73 @@ export const FOOD = [
         id: 0,
         name: "Plain Bagel",
         image: '',
-        description: '',
-        price: 5
-    }
+        price: "3"
+    },
+    {
+        id: 1,
+        name: "Chocolate Croissant",
+        image: '',
+        price: "4"
+    },
+    {
+        id: 2,
+        name: "Glazed Donut",
+        image: '',
+        price: "3"
+    },
+    {
+        id: 3,
+        name: "Almond Croissant",
+        image: '',
+        price: "4"
+    },
+    {
+        id: 4,
+        name: "Brownie",
+        image: '',
+        price: "3"
+    },
+    {
+        id: 5,
+        name: "Ham & Cheese Croissant",
+        image: '',
+        price: "5"
+    },
+    {
+        id: 6,
+        name: "Cookies",
+        image: '',
+        price: "2"
+    },
+    {
+        id: 7,
+        name: "Scones",
+        image: '',
+        price: "3"
+    },
+    {
+        id: 8,
+        name: "Chocolate Donut",
+        image: '',
+        price: "3"
+    },
+    {
+        id: 9,
+        name: "Blueberry Bagel",
+        image: '',
+        price: "3"
+    },
+    {
+        id: 10,
+        name: "Chocolate Bagel",
+        image: '',
+        price: "3"
+    },
+    {
+        id: 11,
+        name: "Muffins",
+        image: '',
+        price: "3"
+    },
 
 ]
